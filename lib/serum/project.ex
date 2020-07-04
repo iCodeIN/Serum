7118e21b779306193747b2d2f59aaac484cc136b
Dalgona <dalgona@hontou.moe>
2020-07-04 18:37:47 +0900

    refactor!(serum): remove defstruct from Serum.Project
    Now new/1 makes a Serum.V2.Project struct.

--- lib/serum/project.ex
+++ lib/serum/project.ex
@@ -1,94 +1,38 @@
 defmodule Serum.Project do
-  @moduledoc """
-  This module defines a struct for storing Serum project metadata.
+  @moduledoc false
+
+  _moduledocp = """
+  Defines functions internally used by Serum to create and manipulate
+  `Serum.V2.Project` structs.
   """
 
   import Serum.V2.Console, only: [put_err: 2]
-  alias Serum.V2.Plugin
-  alias Serum.V2.Theme
-
-  @default_date_format "{YYYY}-{0M}-{0D}"
-  @default_list_title_tag "Posts Tagged ~s"
-
-  defstruct site_name: "",
-            site_description: "",
-            server_root: "",
-            base_url: "",
-            author: "",
-            author_email: "",
-            date_format: @default_date_format,
-            list_title_all: "All Posts",
-            list_title_tag: @default_list_title_tag,
-            pagination: false,
-            posts_per_page: 5,
-            preview_length: 200,
-            src: nil,
-            dest: nil,
-            plugins: [],
-            theme: nil
-
-  @type t :: %__MODULE__{
-          src: binary(),
-          dest: binary(),
-          site_name: binary(),
-          site_description: binary(),
-          server_root: binary(),
-          base_url: binary(),
-          author: binary(),
-          author_email: binary(),
-          date_format: binary(),
-          list_title_all: binary(),
-          list_title_tag: binary(),
-          pagination: boolean(),
-          posts_per_page: pos_integer(),
-          preview_length: non_neg_integer(),
-          plugins: [Plugin.spec()],
-          theme: Theme.spec() | nil
-        }
+  alias Serum.V2.Project
+  alias Serum.V2.Project.BlogConfiguration
 
   @spec default_date_format() :: binary()
-  def default_date_format, do: @default_date_format
+  def default_date_format, do: "{YYYY}-{0M}-{0D}"
 
   @spec default_list_title_tag() :: binary()
-  def default_list_title_tag, do: @default_list_title_tag
+  def default_list_title_tag, do: "Posts Tagged ~s"
 
-  @doc "A helper function for creating a new Project struct."
-  @spec new(map) :: t
+  @spec new(map) :: Project.t()
   def new(map) do
-    checked_map =
-      map
-      |> check_date_format()
-      |> check_list_title_format()
-
-    struct(__MODULE__, checked_map)
+    struct(Project, put_blog_config(map))
   end
 
-  @spec check_date_format(map) :: map
-  defp check_date_format(map) do
-    case map[:date_format] do
-      nil ->
-        map
+  @spec put_blog_config(map()) :: map()
+  defp put_blog_config(map)
 
-      fmt when is_binary(fmt) ->
-        case Timex.validate_format(fmt) do
-          :ok ->
-            map
-
-          {:error, message} ->
-            msg = """
-            Invalid date format string `date_format`:
-              #{message}
-            The default format string will be used instead.
-            """
-
-            put_err(:warn, String.trim(msg))
-            Map.delete(map, :date_format)
-        end
-    end
+  defp put_blog_config(%{blogs: %{} = blog_config} = map) do
+    %{map | blogs: struct(BlogConfiguration, check_list_title_tag(blog_config))}
   end
 
-  @spec check_list_title_format(map) :: map
-  defp check_list_title_format(map) do
+  defp put_blog_config(%{blogs: false} = map), do: map
+  defp put_blog_config(map), do: map
+
+  @spec check_list_title_tag(map) :: map
+  defp check_list_title_tag(map) do
     case map[:list_title_tag] do
       nil ->
         map
